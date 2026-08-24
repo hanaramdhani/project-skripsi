8e1f5cc24897a660e325cb0e0741a49341f34e47
fransdika <fransdika94@gmail.com>
2026-08-24 05:26:45 +0100

    ganti header modal mbs

--- resources/views/BarangSatuan.blade.php
+++ resources/views/BarangSatuan.blade.php
@@ -59,7 +59,7 @@
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Form Edit Pegawai</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Form Edit Barang Satuan</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
                             </button>
